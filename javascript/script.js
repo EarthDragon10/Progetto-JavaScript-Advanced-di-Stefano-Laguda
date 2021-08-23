819eb4611667a25cbbcb0d131149db855809ee13
earthdragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-08-23 19:55:10 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -8,31 +8,84 @@ const errorCallback = (error) => {
     console.error(error);
 };
 
-navigator.geolocation.getCurrentPosition( successCallback, errorCallback, {
+let coordinate = navigator.geolocation.getCurrentPosition( successCallback, errorCallback, {
     enableHighAccuracy: true,
     timeout: 5000
 })
 
-// -----Inizializzazione dell'API di AQICN----------
+// ---- Creazione degli agganci -----
+let button = document.getElementById("btn");
 
-let btn = document.getElementById("btn");
 
-btn.onclick = () => {
-    let city = document.getElementById("input").value;
-    let apiKey = "635c09a92e47deb3be9fe65861ce2778b99705e7";
-    let url = "https://api.waqi.info/feed/" + city + "/?token=" + apiKey;
+let geoUser = document.getElementById("geo");
 
-    // Setto la richiesta da inviare al server
+let cityDisplay = document.getElementById("city");
+let pmDisplay = document.getElementById("pm");
+let aqiDisplay = document.getElementById("aqi");
+let healthDisplay = document.getElementById("health");
+
+
+// -----Inizializzazione dell'API -----
+
+button.onclick = function() {
+
+    let apiKey = "69ab781cd90f5b1d86bdd86a5be7105b2a946da3";
+    let inputCity = document.getElementById("input").value;
+    let url = "https://api.waqi.info/feed/" + inputCity + "/?token=" + apiKey;
+
+    // Inizializzo la richiesta da inviare al server
     var request = new XMLHttpRequest();
     request.open("GET", url, true);
     request.onload = function() {
 
         if(request.status >= 200 && request.status < 400) {
             var data = JSON.parse(this.response);
+            var cityUser = data.data.city.name;
+            var pm25 = data.data.iaqi.pm25.v;
+            var health = data.data.aqi;
+
             console.log(data);
-            var città = data.city.name;
-            console.log(città);
+            console.log(cityUser);
+            console.log(pm25);
+            console.log(health);
+
+            cityDisplay.innerHTML = cityUser;
+            pmDisplay.innerHTML = pm25;
+            aqiDisplay.innerHTML = health;
+
+            if(health >= 0 && health <= 50) {
+                healthDisplay.innerHTML = "Good!";
+            } else if(health >= 51 && health <= 100) {
+                healthDisplay.innerHTML = "Moderate";
+            } else if(health >= 101 && health <= 150) {
+                healthDisplay.innerHTML = "Unhealthy for Sensitive Groups";
+            } else if(health >= 151 && health <= 200) {
+                healthDisplay.innerHTML = "Unhealthy";
+            } else if(health >= 201 && health <= 300) {
+                healthDisplay.innerHTML = "Very Unhealthy!";
+            } else {
+                healthDisplay.innerHTML = "Hazardous";
+            }
         }
-    }
+    };
+
     request.send();
 }
+
+geoUser.onclick = function() {
+    let apiKey = "69ab781cd90f5b1d86bdd86a5be7105b2a946da3";
+    let url = "https://api.waqi.info/feed/geo" + lat + "" + lng + "/?token=" + apiKey;
+
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.onload = function() {
+        if(request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(this.response);
+            
+        }
+    }
+
+    request.send();
+}
+
+
