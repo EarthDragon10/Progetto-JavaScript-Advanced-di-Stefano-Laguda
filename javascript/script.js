1aa191a1e5a83120007220a544ba59d9edccba66
earthdragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-08-24 12:05:38 +0200

    ultimazione per la geolocalizzazione

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,4 @@
 // Uso della getCurrentPosition per calcolare la posizione dell'utente
-
-const successCallback = (position) => {
-    console.log(position);
-};
-
-const errorCallback = (error) => {
-    console.error(error);
-};
-
-let coordinate = navigator.geolocation.getCurrentPosition( successCallback, errorCallback, {
-    enableHighAccuracy: true,
-    timeout: 5000
-})
-
 // ---- Creazione degli agganci -----
 let button = document.getElementById("btn");
 
@@ -72,20 +58,79 @@ button.onclick = function() {
     request.send();
 }
 
+// const successCallback = (position) => {
+//     console.log(position);
+//     var lat = `${pos.coords.latitude}`;
+//     var lng = `${pos.coords.longitude}`;
+// };
+
+// const errorCallback = (error) => {
+//     console.error(error);
+// };
+
+// navigator.geolocation.getCurrentPosition( successCallback, errorCallback, {
+//     enableHighAccuracy: true,
+//     timeout: 5000,
+//     maximumAge: 0
+// });
+
+
+
 geoUser.onclick = function() {
     let apiKey = "69ab781cd90f5b1d86bdd86a5be7105b2a946da3";
-    let url = "https://api.waqi.info/feed/geo" + lat + "" + lng + "/?token=" + apiKey;
+
+    let lat;
+    let lng;
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        var crd = position.coords;
+
+        lat = crd.latitude;
+        lng = crd.longitude;
+
+        let url = `https://api.waqi.info/feed/geo:${lat};${lng}/?token=${apiKey}`;
 
     var request = new XMLHttpRequest();
     request.open("GET", url, true);
+    
     request.onload = function() {
+
         if(request.status >= 200 && request.status < 400) {
             var data = JSON.parse(this.response);
-            
-        }
-    }
+            console.log(data);
+            var cityUser = data.data.city.name;
+            var pm25 = data.data.iaqi.pm25.v;
+            var health = data.data.aqi;
 
+            console.log(data);
+            console.log(cityUser);
+            console.log(pm25);
+            console.log(health);
+
+            cityDisplay.innerHTML = cityUser;
+            pmDisplay.innerHTML = pm25;
+            aqiDisplay.innerHTML = health;
+
+            if(health >= 0 && health <= 50) {
+                healthDisplay.innerHTML = "Good!";
+            } else if(health >= 51 && health <= 100) {
+                healthDisplay.innerHTML = "Moderate";
+            } else if(health >= 101 && health <= 150) {
+                healthDisplay.innerHTML = "Unhealthy for Sensitive Groups";
+            } else if(health >= 151 && health <= 200) {
+                healthDisplay.innerHTML = "Unhealthy";
+            } else if(health >= 201 && health <= 300) {
+                healthDisplay.innerHTML = "Very Unhealthy!";
+            } else {
+                healthDisplay.innerHTML = "Hazardous";
+            }
+        }
+    };
     request.send();
+    
+    });
+
+    
 }
 
 
