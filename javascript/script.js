2e788251c52bc528fd2595256316ab91066a9e3d
earthdragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-08-24 22:02:42 +0200

    alcune review

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,40 @@
-// Uso della getCurrentPosition per calcolare la posizione dell'utente
+// ►►►► Sezione HTML realizzata con JavaScript ◄◄◄◄
+
+// 1. Sezione Creazione degli elementi HTML
+
+let mainSite = document.createElement("main");
+let headerSite = document.createElement("header");
+let sectionSite = document.createElement("section");
+let formSite = document.createElement("form");
+
+// Elementi dell'header
+let h1 = document.createElement("h1");
+let p = document.createElement("p");
+
+// Elementi del Form
+let input = document.createElement("input");
+let btn = document.createElement("button");
+let iconItem = document.createElement("i");
+
+// Elementi che mostrano i risultati della ricerca
+let div = document.createElement("div");
+let h3 = document.createElement("h3");
+
+// 2. Costruzione dell'Header
+h1.innerHTML = "Enter city or country";
+p.innerHTML = "Check the air pollution in your city/country";
+
+document.body.prepend(mainSite);
+mainSite.prepend(sectionSite);
+sectionSite.prepend(headerSite);
+headerSite.prepend(h1);
+h1.after(p);
+
+
+
+
+
+
 // ---- Creazione degli agganci -----
 let button = document.getElementById("btn");
 
@@ -11,7 +47,7 @@ let aqiDisplay = document.getElementById("aqi");
 let healthDisplay = document.getElementById("health");
 
 
-// -----Inizializzazione dell'API -----
+// -----Input Classico -----
 
 button.onclick = function() {
 
@@ -58,23 +94,8 @@ button.onclick = function() {
     request.send();
 }
 
-// const successCallback = (position) => {
-//     console.log(position);
-//     var lat = `${pos.coords.latitude}`;
-//     var lng = `${pos.coords.longitude}`;
-// };
 
-// const errorCallback = (error) => {
-//     console.error(error);
-// };
-
-// navigator.geolocation.getCurrentPosition( successCallback, errorCallback, {
-//     enableHighAccuracy: true,
-//     timeout: 5000,
-//     maximumAge: 0
-// });
-
-
+//------ Geolocalizzazione ------
 
 geoUser.onclick = function() {
     let apiKey = "69ab781cd90f5b1d86bdd86a5be7105b2a946da3";
@@ -90,47 +111,45 @@ geoUser.onclick = function() {
 
         let url = `https://api.waqi.info/feed/geo:${lat};${lng}/?token=${apiKey}`;
 
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    
-    request.onload = function() {
+        var request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        
+        request.onload = function() {
 
-        if(request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(this.response);
-            console.log(data);
-            var cityUser = data.data.city.name;
-            var pm25 = data.data.iaqi.pm25.v;
-            var health = data.data.aqi;
+            if(request.status >= 200 && request.status < 400) {
+                var data = JSON.parse(this.response);
+                console.log(data);
+                var cityUser = data.data.city.name;
+                var pm25 = data.data.iaqi.pm25.v;
+                var health = data.data.aqi;
 
-            console.log(data);
-            console.log(cityUser);
-            console.log(pm25);
-            console.log(health);
+                console.log(data);
+                console.log(cityUser);
+                console.log(pm25);
+                console.log(health);
 
-            cityDisplay.innerHTML = cityUser;
-            pmDisplay.innerHTML = pm25;
-            aqiDisplay.innerHTML = health;
+                cityDisplay.innerHTML = cityUser;
+                pmDisplay.innerHTML = pm25;
+                aqiDisplay.innerHTML = health;
 
-            if(health >= 0 && health <= 50) {
-                healthDisplay.innerHTML = "Good!";
-            } else if(health >= 51 && health <= 100) {
-                healthDisplay.innerHTML = "Moderate";
-            } else if(health >= 101 && health <= 150) {
-                healthDisplay.innerHTML = "Unhealthy for Sensitive Groups";
-            } else if(health >= 151 && health <= 200) {
-                healthDisplay.innerHTML = "Unhealthy";
-            } else if(health >= 201 && health <= 300) {
-                healthDisplay.innerHTML = "Very Unhealthy!";
-            } else {
-                healthDisplay.innerHTML = "Hazardous";
+                if(health >= 0 && health <= 50) {
+                    healthDisplay.innerHTML = "Good!";
+                } else if(health >= 51 && health <= 100) {
+                    healthDisplay.innerHTML = "Moderate";
+                } else if(health >= 101 && health <= 150) {
+                    healthDisplay.innerHTML = "Unhealthy for Sensitive Groups";
+                } else if(health >= 151 && health <= 200) {
+                    healthDisplay.innerHTML = "Unhealthy";
+                } else if(health >= 201 && health <= 300) {
+                    healthDisplay.innerHTML = "Very Unhealthy!";
+                } else {
+                    healthDisplay.innerHTML = "Hazardous";
+                }
             }
-        }
-    };
-    request.send();
-    
-    });
+        };
 
-    
+        request.send();
+        });
 }
 
 
